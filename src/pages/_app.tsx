f7ae0b38a2fd743af7e578b7b15e149b9fc5bfc9
Owen Rummage <owen@rummage.cc>
2024-12-31 19:22:27 -0600

    Fix favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           content="NixLabs Networks statuspage showing all service stats in real time!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          href="https://nixlabs.dev/logo.svg"
+          type="image/svg+xml"
+        />
       </Head>
       <div className="antialiased flex flex-col items-center text-white lg:gap-8 py-4 bg-[#030303] dark min-h-screen">
         <Header />
